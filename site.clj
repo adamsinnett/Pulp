a9f11a886a6601bda0e4ed5f8424d8f11ddfa9d0
Adam Sinnett <quandrum@gmail.com>
2012-07-04 16:21:31 -0700

    Base file handling

---
+++ site.clj
@@ -0,0 +1,10 @@
+(ns pulp.site
+  "Generate a static site given a source directory of templates"
+  (:require
+    [clojure.java.io :as io])
+  (:import [java.io File]))
+
+(defn compile-site
+  "Create a site from a given source directory."
+  [source]
+  ())
--- src/pulp/core.clj
+++ src/pulp/core.clj
@@ -1,1 +1,27 @@
-(ns pulp.core)
+(ns pulp.core
+  "Pulp: Static Site Generator"
+  (:use
+    [pulp site])
+  (:require
+    [clojure.java.io :as io])
+  (:import [java.io File]))
+
+
+(defn gen-site
+  "Generate a static site"
+  [source dest]
+  (write-site 
+    (compile-site source)
+    dest))
+
+(defn write-site
+  "Write the generated site into a destination folder"
+  [site dest]
+  (map write-page site))
+
+(defn write-page
+  "Write the generated page into the destination folder"
+  [page]]
+  (with-open [w (io/writer (:title page))]
+    (spit w (:data page)))
+
