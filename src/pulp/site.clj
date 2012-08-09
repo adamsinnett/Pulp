17e2b95e94316849dcbf26448ee343a18131d83a
Adam Sinnett <sinneta@webtrends.com>
2012-08-09 11:43:52 -0700

    Does it work?

--- src/pulp/site.clj
+++ src/pulp/site.clj
@@ -6,19 +6,15 @@
     [clojure.java.io :as io])
   (:import [java.io File]))
 
-(defn compile-pages
-  "Create html file from a template"
-  [template-file]
-  (html template-file))
 
 (defn get-template-files
   "Return all the file from a given directory"
   [source-dir]
-  (file-seq (File. source-dir)))
+  (filter .isFile (file-seq (File. source-dir))))
 
 (defn compile-site
   "Create a site from a given source directory."
   [source-dir]
   (let [all-files (get-template-files source-dir)
         template-files (filter #(re-matches #"\.html" %) (.getName all-files))]
-    (map compile-pages template-files)))
+    (print template-files)))
